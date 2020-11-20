5d5dc1fce5a658003e9202b08d6b742b08327146
ddayooung <dimmaculatey@gmail.com>
2020-11-20 18:08:15 +0900

    Fix: css 안먹던 문제 수정

--- js/Slider.js
+++ js/Slider.js
@@ -1,66 +1,87 @@
-const Slider = function () {
-  const sliderInnerWrap = document.querySelector('.slider-innerWrap');
-  const slider = sliderInnerWrap.querySelector('.slider');
-  const slideLis = slider.querySelectorAll('li');
-  const sliderBtn = document.querySelector('.slider_wrap .slider_btn');
-  slider.addEventListener('transitionend', resetSlider);
-  sliderBtn.addEventListener('click', moveSlider);
+const Slider = function (target) {
+  window.onload = () => {
+    // 사용자가 Slider 호출하면 slider 동작에 필요한 tag들 생성
+    const slider = document.querySelector(target);
+    const slideLis = slider.querySelectorAll('li');
+    // wrapper 생성
+    const sliderInnerWrap = document.createElement('div');
+    const sliderWrap = document.createElement('div');
+    slider.parentNode.insertBefore(sliderWrap, slider);
+    sliderInnerWrap.appendChild(slider);
+    sliderWrap.appendChild(sliderInnerWrap);
+    sliderInnerWrap.className = 'slider_innerWrap';
+    sliderWrap.className = 'slider_wrap';
+    // button 생성
+    const sliderBtn = document.createElement('div');
+    const prev = document.createElement('button');
+    const next = document.createElement('button');
+    sliderBtn.appendChild(prev);
+    sliderBtn.appendChild(next);
+    sliderWrap.appendChild(sliderBtn);
+    sliderBtn.className = 'slider_btn';
+    prev.className = 'prev';
+    next.className = 'next';
+    prev.textContent = '<';
+    next.textContent = '>';
 
-  const clonedFirstLi = slideLis[0].cloneNode(true);
-  const clonedLastLi = slideLis[slideLis.length - 1].cloneNode(true);
-  slider.prepend(clonedLastLi);
-  slider.appendChild(clonedFirstLi);
+    slider.addEventListener('transitionend', resetSlider);
+    sliderBtn.addEventListener('click', moveSlider);
+    const clonedFirstLi = slideLis[0].cloneNode(true);
+    const clonedLastLi = slideLis[slideLis.length - 1].cloneNode(true);
+    slider.prepend(clonedLastLi);
+    slider.appendChild(clonedFirstLi);
 
-  const cloneIncludedLis = slider.querySelectorAll('li');
-  const liWidth = cloneIncludedLis[0].clientWidth;
-  const sliderWidth = liWidth * cloneIncludedLis.length;
-  sliderInnerWrap.style.width = liWidth + 'px';
-  slider.style.width = sliderWidth + 'px';
+    const cloneIncludedLis = slider.querySelectorAll('li');
+    const liWidth = cloneIncludedLis[0].clientWidth;
+    const sliderWidth = liWidth * cloneIncludedLis.length;
+    sliderInnerWrap.style.width = liWidth + 'px';
+    slider.style.width = sliderWidth + 'px';
 
-  const INIT_POSITION = -liWidth;
-  const INIT_INDEX = 1;
-  let leftPosition = INIT_POSITION;
-  let currentIndex = INIT_INDEX;
-  let transitionDuration = 300;
-  slider.style.transition = `all ${transitionDuration}ms ease`;
-  slider.style.left = leftPosition + 'px';
-
-  function moveSlider(e) {
+    const INIT_POSITION = -liWidth;
+    const INIT_INDEX = 1;
+    let leftPosition = INIT_POSITION;
+    let currentIndex = INIT_INDEX;
+    let transitionDuration = 300;
     slider.style.transition = `all ${transitionDuration}ms ease`;
-    preventOverClicks(e.target);
-    if (e.target.className === 'next') {
-      moveSliderLeft(-1);
-    }
-    if (e.target.className === 'prev') {
-      moveSliderLeft(1);
+    slider.style.left = -liWidth + 'px';
+
+    function moveSlider(e) {
+      slider.style.transition = `all ${transitionDuration}ms ease`;
+      preventOverClicks(e.target);
+      if (e.target.className === 'next') {
+        moveSliderLeft(-1);
+      }
+      if (e.target.className === 'prev') {
+        moveSliderLeft(1);
+      }
+
+      function moveSliderLeft(direction) {
+        currentIndex += -1 * direction;
+        leftPosition += liWidth * direction;
+        slider.style.left = leftPosition + 'px';
+      }
+
+      function preventOverClicks(targetBtn) {
+        targetBtn.setAttribute('disabled', true);
+        setTimeout(() => targetBtn.removeAttribute('disabled'), transitionDuration);
+      }
     }
 
-    function moveSliderLeft(direction) {
-      currentIndex += -1 * direction;
-      leftPosition += liWidth * direction;
-      slider.style.left = leftPosition + 'px';
-    }
+    function resetSlider() {
+      if (currentIndex === cloneIncludedLis.length - 1) {
+        resetSliderManager(1);
+      }
+      if (currentIndex === 0) {
+        resetSliderManager(cloneIncludedLis.length - 2);
+      }
 
-    function preventOverClicks(target) {
-      target.setAttribute('disabled', true);
-      setTimeout(() => target.removeAttribute('disabled'), transitionDuration);
+      function resetSliderManager(nextIndex) {
+        currentIndex = nextIndex;
+        leftPosition = -liWidth * nextIndex;
+        console.log(leftPosition);
+        slider.style.left = leftPosition + 'px';
+        slider.style.transition = 'none';
+      }
     }
-  }
-
-  function resetSlider() {
-    if (currentIndex === cloneIncludedLis.length - 1) {
-      resetSliderManager(1);
-    }
-    if (currentIndex === 0) {
-      resetSliderManager(cloneIncludedLis.length - 2);
-    }
-
-    function resetSliderManager(nextIndex) {
-      currentIndex = nextIndex;
-      leftPosition = -liWidth * nextIndex;
-      console.log(leftPosition);
-      slider.style.left = leftPosition + 'px';
-      slider.style.transition = 'none';
-    }
-  }
+  };
 };
